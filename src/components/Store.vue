f6a966a23e49b8b3d20e3e0f3ff3abf3542a3987
gabrielblank <gabrielpbbl4nk@gmail.com>
2024-10-28 21:22:17 -0300

    create store component

---
+++ src/components/Store.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['store'])
+</script>
+
+<template>
+    <div class="store-container">
+        <h1>{{ store.nome }}</h1>
+        <h2>{{ store.endereco }}</h2>
+    </div>
+</template>
+
+<style scoped>
+.store-container {
+    background-color: aqua;
+    margin: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,34 @@
 <script setup>
 import HeaderComponent from '../components/Header.vue'
 import CreateStoreModal from '../components/CreateStoreModal.vue'
-import { ref } from 'vue';
+import Store from '@/components/Store.vue'
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
 
+onMounted(() => {
+    // const result = axios('http://localhost:3000/mercados', {
+    //     method: 'GET'
+    // })
+    // stores.value = result
+})
+
+const stores = ref([
+    {
+        id: 1,
+        nome: 'teste1',
+        endereco: 'teste11'
+    },
+    {
+        id: 2,
+        nome: 'teste2',
+        endereco: 'teste22'
+    },
+    {
+        id: 3,
+        nome: 'teste3',
+        endereco: 'teste33'
+    }
+])
 const showModal = ref(false)
 
 function openModal(value) {
@@ -15,6 +41,10 @@ function openModal(value) {
     <CreateStoreModal v-if="showModal" @modal="openModal"/>
 
     <HeaderComponent title="LOJAS" btnText="Adicionar lojas" @modal="openModal"/>
+
+    <div class="stores-container">
+        <Store v-for="s in stores" :key="s.id" :store="s"/>
+    </div>
 </template>
 
 <style scoped>
