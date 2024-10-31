6b776abf529ebeed4b0b621cf2633e1271f5f531
gabrielblankk <gabrielpbbl4nk@gmail.com>
2024-10-31 15:27:10 -0300

    create product info view

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,15 +1,16 @@
 <script setup>
-  import HeaderComponent from '../components/Header.vue'
+  import { Icon } from '@iconify/vue';
+import HeaderComponent from '../components/Header.vue'
   import axios from 'axios'
 import { useRoute } from 'vue-router'
+import { useRouter }  from 'vue-router'
 
+const router = useRouter()
 const route = useRoute()
-
-//const product = axios(`http://localhost:3000/produto/${route.params.id}`)
 
 const product = {
   nome: 'Teste produto aa',
-  descricao: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+  descricao: 'um produto qualquer ai aaaaaaaaaaaaaaaa',
   preco: '123123',
   quantidade: '84'
 }
@@ -18,11 +19,94 @@ const product = {
 
 <template>
   <HeaderComponent :title="product.nome" btnText=""/>
-  <h1>{{ product.descricao }}</h1>
-  <h1>{{ product.preco }}</h1>
-  <h1>{{ product.quantidade }}</h1>
+
+  <div class="product-info-container">
+    <div class="top-container">
+      <div class="image"> <Icon icon="material-symbols:imagesmode-rounded" color="#555" width="50px"/> </div>
+      
+      <div class="info-container">
+        <div clas="infos">
+          <h1 class="price">R${{ product.preco }}</h1>
+      
+          <p class="description">{{ product.descricao }}</p>
+        </div>
+    
+        <div class="quantity-container">
+          <p class="quantity">{{ product.quantidade }}<span> em estoque</span></p>
+        </div>
+      </div>
+    </div>
+      
+    <button @click="router.push(`/${route.params.id}/movimentacoes`)" class="moves-btn">Ver movimentações do produto</button>
+  </div>
 </template>
 
 <style scoped>
+.product-info-container {
+  width: 60%;
+  margin: 50px auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
 
+.image {
+  background-color: #ccc;
+  width: 300px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  filter: drop-shadow(0px 0px 5px #ccc);
+}
+
+.top-container {
+  display: flex;
+  width: 80%;
+}
+
+.info-container {
+  margin-left: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.price {
+  font-size: 50px;
+}
+
+.description {
+  font-size: 20px;
+}
+
+.quantity {
+  font-size: 40px;
+}
+
+.quantity span {
+  font-size: 20px;
+}
+
+button {
+  margin-top: 100px;
+  background-color: #ccc;
+  border: none;
+  border-radius: 10px;
+  width: 80%;
+  font-size: 20px;
+  padding: 20px;
+  filter: drop-shadow(0px 0px 5px #ccc);
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+button:hover {
+  transform: scale(1.02);
+}
+
+button:active {
+  transform: scale(0.98);
+}
 </style>
