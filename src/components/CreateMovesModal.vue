8c3a2558ae6615c9b539e25fbcb040f2375a836f
gabrielblankk <gabrielpbbl4nk@gmail.com>
2024-10-31 17:43:50 -0300

    edit moves component visual and add date

--- src/components/CreateMovesModal.vue
+++ src/components/CreateMovesModal.vue
@@ -4,125 +4,124 @@ import axios from 'axios'
 
 const emit = defineEmits(['modal'])
 
-
 const type = ref('')
 const quantity = ref('')
 const inputColor = ref('solid 1px black')
 
 function validateInputs() {
-    if (type.value === '' || quantity.value == ''
-    ) {
-        inputColor.value = 'solid 1px red'
-    } else {
-        inputColor.value = 'solid 1px black'
+	if (type.value === '' || quantity.value == '') {
+		inputColor.value = 'solid 1px red'
+	} else {
+		inputColor.value = 'solid 1px black'
 
-        const movement = {
-            tipo: type.value,
-            quantidade: quantity.value
-        }
+		const movement = {
+			tipo: type.value,
+			quantidade: quantity.value,
+			data: new Date().toLocaleDateString()
+		}
 
-        axios('http://localhost:3000/movimentos', {
-            method: 'POST',
-            data: movement
-        })
+		axios('http://localhost:3000/movimentos', {
+			method: 'POST',
+			data: movement
+		})
 
-        emit('modal', false)
-    }
+		emit('modal', false)
+	}
 }
-
 </script>
 
 <template>
-    <div @click="emit('modal', false)" class="overlay"></div>
+	<div @click="emit('modal', false)" class="overlay"></div>
 
-    <div class="modal">
-        <h1>Novo Movimento</h1>
+	<div class="modal">
+		<h1>Novo Movimento</h1>
 
-        <div class="line"></div>
+		<div class="line"></div>
 
-        <div class="input-container">
-            <label>Tipo</label>
-            <select v-model="type" :style="{border: inputColor}">
-                <option value="" selected disabled hidden></option>
-                <option value="entrada">Entrada</option>
-                <option value="saida">Saída</option>
-            </select>
+		<div class="input-container">
+			<label>Tipo</label>
+			<select v-model="type" :style="{ border: inputColor }">
+				<option value="" selected disabled hidden></option>
+				<option value="entrada">Entrada</option>
+				<option value="saida">Saída</option>
+			</select>
 
-            <label>Quantidade</label>
-            <input v-model="quantity" :style="{border: inputColor}" type="number">
-        </div>
+			<label>Quantidade</label>
+			<input v-model="quantity" :style="{ border: inputColor }" type="number">
+		</div>
 
-        <button @click="validateInputs" class="btn">Criar Movimento</button>
-    </div>
+		<button @click="validateInputs" class="btn">Criar Movimento</button>
+	</div>
 </template>
 
 <style scoped>
 .overlay {
-    position: fixed;
-    z-index: 10;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 50%);
+	position: fixed;
+	z-index: 10;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 50%);
 }
 
 .modal {
-    width: 50%;
-    height: 70%;
-    padding: 40px;
-    background-color: #eee;
-    position: fixed;
-    border-radius: 20px;
-    z-index: 11;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: space-around;
+	width: 50%;
+	height: 70%;
+	padding: 40px;
+	background-color: #eee;
+	position: fixed;
+	border-radius: 20px;
+	z-index: 11;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: space-around;
 }
 
 .line {
-    width: 80%;
-    height: 5px;
-    background-color: #ccc;
-    border-radius: 100%;
+	width: 80%;
+	height: 5px;
+	background-color: #ccc;
+	border-radius: 100%;
 }
 
 .input-container {
-    display: flex;
-    flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 
 h1 {
-    font-size: 60px;
+	font-size: 60px;
 }
 
-input, select {
-    font-size: 26px;
-    padding: 5px;
-    border-radius: 10px;
+input,
+select {
+	font-size: 26px;
+	padding: 5px;
+	border-radius: 10px;
 }
 
 label {
-    font-size: 30px;
+	font-size: 30px;
 }
 
 button {
-    padding: 20px 50px;
-    font-size: 30px;
-    border: none;
-    border-radius: 20px;
-    background-color: #ccc;
-    transition: 0.2s;
-    cursor: pointer;
+	padding: 20px 50px;
+	font-size: 30px;
+	border: none;
+	border-radius: 20px;
+	background-color: #ccc;
+	transition: 0.2s;
+	cursor: pointer;
 }
 
 button:hover {
-    transform: scale(1.05);
+	transform: scale(1.05);
 }
 
 button:active {
-    transform: scale(0.95);
+	transform: scale(0.95);
 }
 </style>
--- src/components/Move.vue
+++ src/components/Move.vue
@@ -5,30 +5,41 @@ const props = defineProps(['move'])
 </script>
 
 <template>
-    <div class="move">
-        <h1>{{ props.move.tipo }}</h1>
-        <h1>{{ props.move.quantidade }}</h1>
-        <h1>{{ props.move.data }}</h1>
+  <div class="move">
+    <div>
+      <h1>{{ props.move.tipo }}</h1>
+
+      <p> <span>{{ props.move.quantidade }}</span> unidades</p>
     </div>
+
+    <p>{{ props.move.data }}</p>
+  </div>
 </template>
 
 <style scoped>
 .move {
-    background-color: #ccc;
-    margin: 10px;
-    border-radius: 10px;
-    padding: 20px;
-    width: 100%;
-    transition: 0.2s;
-    filter: drop-shadow(0px 0px 5px #ccc);
-    cursor: pointer;
+  background-color: #ccc;
+  margin: 10px;
+  border-radius: 10px;
+  padding: 20px;
+  width: 100%;
+  transition: 0.2s;
+  filter: drop-shadow(0px 0px 5px #ccc);
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 p {
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .move:hover {
-    transform: scale(1.02);
+  transform: scale(1.02);
+}
+
+span {
+  font-size: 30px;
 }
 </style>
